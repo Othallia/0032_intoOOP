0b38864b6efc5b7ecf79d4665b970ca433f7da0d
Othallia <vianiputriocta6@gmail.com>
2024-04-23 17:33:17 +0700

    menambahkan void output

--- 0032_intoOOP/0032_intoOOP.cpp
+++ 0032_intoOOP/0032_intoOOP.cpp
@@ -6,4 +6,9 @@ public: // akses modifier
 	string nama;
 	int umur;
 	string jurusan;
-
+	void output() {
+		cout << "Nama : " << nama << endl;
+		cout << "Umur : " << umur << endl;
+		cout << "Jurusan : " << jurusan << endl;
+	}
+};
